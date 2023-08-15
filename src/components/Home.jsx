c6acec9ae65f2eb88add5688941281e76c3ad817
CodeGoutam <hgoutam2001@gmail.com>
2023-08-15 23:58:04 +0530

    Indentation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Home() {
     return(
 <>
-<div style={{backgroundColor:'#0C356A',height:'fitContent',padding:'1rem'}}><h1 style={{textAlign:'center',color:'white'}}>Image Gallary</h1></div>
+<div style={{margin:'1rem',backgroundColor:'#0C356A',height:'fitContent',padding:'1rem'}}><h1 style={{textAlign:'center',color:'white'}}>Image Gallary</h1></div>
 </>
 
     );
